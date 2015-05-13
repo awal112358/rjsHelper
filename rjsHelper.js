f77c954d17c58a6e482f85ac004e080e1593aeca
Andrew Walker <awal112358@gmail.com>
2015-05-13 08:40:02 -0500

    abstracting a bit more and fixing some path issues

--- rjsHelper.js
+++ rjsHelper.js
@@ -1,19 +1,29 @@
+'use strict';
+
 var path = require('path');
+var fs = require('fs');
 
 //requireJS helper object
 //  needed so that this can be computed at the task runtime instead of
 //  on config/load.  The only way to guarantee run-time accuracy is to
 //  write a main file that gets loaded in via grunt's require task instead
 //  of using the built in options which are analyzed at grunt config init
-module.exports = function(grunt) {
+module.exports = function(grunt, root) {
 
+    //validate root
+    root = fs.realpathSync(root);
+    if(!fs.existsSync(root)) {
+        throw new Error("rjsHelper requires a valid root directory! Could not locate: "+root);
+    }
+
+    this._srcRoot = root;
     this._paths = [];
     this._pathMap = {};
     this._shim = {};
     this._srcMatchers = [];
 
     this._getHandle = function(filepath) {
-        return filepath.replace(/\.js$/,'');
+        return filepath.replace(/\.js$/,'').replace(/^\.\//,'');
     };
 
     this.setShim = function(shimConfig) {
@@ -26,35 +36,43 @@ module.exports = function(grunt) {
 
     this._generateFiles = function() {
         if(!this._srcMatchers) {
-            throw new Error("generateFiles() error: setSrcMatchers() must be called first!")
+            throw new Error("generateFiles() error: setSrcMatchers() must be called first!");
         }
         if(!this._shim) {
-            throw new Error("generateFiles() error: setShim() must be called first!")
+            throw new Error("generateFiles() error: setShim() must be called first!");
         }
 
         //generate files from srcMatchers and shim provided
         var shimFiles = Object.keys(this._shim).map(function(val) {
-            return './'+val+'.js';
+            //ensure that file exists
+            var absolutePath = root+path.sep+val+'.js';
+            if(!fs.existsSync(absolutePath)) {
+                throw new Error("filePath not found: "+absolutePath);
+            }
+
+            return './'+path.relative(root,absolutePath);
         });
         var matcherFiles = grunt.file.expand({
-            cwd: './client'
+            cwd: root
         }, this._srcMatchers);
 
-        return shimFiles.concat(matcherFiles);
+        //return the combination of files provided in shim as well as dynamically located matched files
+        return this._paths = shimFiles.concat(matcherFiles);
     };
 
     this._generatePaths = function() {
+        var files = this._paths || this._generateFiles();
 
-        var files = this._generateFiles();
         for (var i=0; i<files.length; i++) {
-            var fileHandle = this._getHandle(files[i]);
-            this._paths.push(fileHandle);
-            this._pathMap[fileHandle] = fileHandle;
+            this._pathMap[this._getHandle(files[i])] = files[i];
         }
     };
 
     this._generateShim = function() {
-        var ngFilesFound = grunt.file.expand(this._srcMatchers);
+        var ngFilesFound = grunt.file.expand({
+                cwd: root
+        }, this._srcMatchers);
+
         for(var i=0; i<ngFilesFound.length; i++) {
             this._shim[this._getHandle(ngFilesFound[i])] = ['init'];
         }
@@ -77,6 +95,6 @@ module.exports = function(grunt) {
             '   });\n' +
             '}).call(this);';
 
-        grunt.file.write(path.resolve(process.cwd()+'/client','require.main.js'),out);
+        grunt.file.write(path.resolve(root,'require.main.js'),out);
     };
 };
